fc1a88836f221318180972fefa78be2e51137e98
Angelica Nascimento <107217901+angelicanascimentoo@users.noreply.github.com>
2023-10-26 11:48:40 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function calcular() {
-    const h0 = Number(document.querySelector("#inputposicao").value); // posicao
-    const v0 = Number(document.querySelector("#inputvelocidade").value); // velcidade
-    const t = Number(document.querySelector("#inputtempo").value); // tempo
+    const h0 = Number(document.querySelector("#posicaoinicial").value); // posicao
+    const v0 = Number(document.querySelector("#velocidadeinicial").value); // velcidade
+    const t = Number(document.querySelector("#tempo").value); // tempo
     const g = 10
     const h = h0 + v0 * t + (g * t ** 2)/ 2 //fórmula
 
-    document.querySelector("#resultado").textContent = "Altura:" + h.toFixed(2) + "m/s"; // oque faz o resultado aparecer
+    document.querySelector("#resultado").textContent = "Altura:" + h.toFixed(2) + "m"; // oque faz o resultado aparecer
    
 }
 
